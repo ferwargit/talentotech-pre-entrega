588f1b5205528325c1da578451ca312e53e0aabe
Fernando Warno <ferwar@hotmail.com>
2025-10-21 03:32:09 -0300

    Requerimiento #2 - POST products <title> <price> <category>

--- index.js
+++ index.js
@@ -16,3 +16,20 @@ if (
   const data = await response.json();
   console.log(data);
 }
+
+if (method.toUpperCase() == "POST" && resource.toLowerCase() == "products") {
+  const newProduct = {
+    title: process.argv[4] || "Default Title",
+    price: process.argv[5] || 0,
+    category: process.argv[6] || "general",
+  };
+  const response = await fetch("https://fakestoreapi.com/products", {
+    method: "POST",
+    body: JSON.stringify(newProduct),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+}
