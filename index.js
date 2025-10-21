05b66d9963ef8d61316d487c7f64b011fe053270
Fernando Warno <ferwar@hotmail.com>
2025-10-21 03:10:46 -0300

    Requerimiento #2 - GET products/id

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ if (method.toUpperCase() == "GET" && resource.toLowerCase() == "products") {
   const data = await response.json();
   console.log(data);
 }
+
+if (
+  method.toUpperCase() == "GET" &&
+  resource.toLowerCase().startsWith("products/")
+) {
+  const id = resource.split("/")[1];
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
