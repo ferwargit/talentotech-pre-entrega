aad861d0a66d82ad5a83d4ea72b65c406afe2e67
Fernando Warno <ferwar@hotmail.com>
2025-10-21 02:50:34 -0300

    Requerimiento #2 - GET products

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hola, mundo!");
+const method = process.argv[2];
+const resource = process.argv[3];
+
+if (method.toUpperCase() == "GET" && resource.toLowerCase() == "products") {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
+  console.log(data);
+}
